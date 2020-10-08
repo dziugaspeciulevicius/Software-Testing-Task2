73f23b6621a7929b3efa266d35c1be8d7fb1fffd
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2020-10-08 21:57:25 +0300

    routing + board + game logic implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
+import Game3x3 from "./components/game/Game3x3.component";
+import Game4x4 from "./components/game/Game4x4.component";
+import Game5x5 from "./components/game/Game5x5.component";
+import NotFound from "./components/notFound/NotFound.component";
+
+import ChooseGame from "./components/chooseGame/ChooseGame.component";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Game3x3} />
+        <Route path="/4x4" component={Game4x4} />
+        <Route path="/5x5" component={Game5x5} />
+        <Route component={NotFound} />
+      </Switch>
+      <ChooseGame />
     </div>
   );
-}
+};
 
 export default App;
